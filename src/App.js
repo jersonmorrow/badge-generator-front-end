872a8c4a190a87975166ad58586593fa31f96503
Jerson Q <solorsanolopez@gmail.com>
2020-12-08 21:57:12 -0800

    [Add] Header setup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './features/auth/login';
 import Register from './features/auth/register';
+import Header from './features/layout/header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
---
+++ src/features/layout/header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+function Header() {
+    return <div>
+        <Link to='./'>
+            <h1>Header to do</h1>
+        </Link>
+        </div>
+}
+
+export default Header
