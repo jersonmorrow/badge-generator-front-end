d9e172ba61baf57033be4583a999c286bc81835c
Jerson Q <solorsanolopez@gmail.com>
2020-12-08 21:41:03 -0800

    [Add] React router authentication path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './features/auth/login';
+import Register from './features/auth/register';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
