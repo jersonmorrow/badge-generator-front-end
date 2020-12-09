4061a681cf51842723866457b4f5fe16c87bfa4a
Jerson Q <solorsanolopez@gmail.com>
2020-12-09 11:21:48 -0800

    [Add] switch login/logout logic context

--- src/features/auth/authOptions/index.jsx
+++ src/features/auth/authOptions/index.jsx
@@ -1,18 +1,34 @@
-import react from 'react'
+import react, { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
+import UserContext from '../../context/userContext' 
 
 function AuthOptions() {
+    const { userData, setUserData } = useContext(UserContext)
+
     const history = useHistory();
 
     const register = () => history.push('./register')
     const login = () => history.push('./login')
+    const logout = () => {
+        setUserData({
+            token: undefined,
+            user: undefined,
+        });
+        localStorage.setItem("auth-token", "")
+    }
 
     return (
         <div>
-        <button onClick={register}>Register</button>
-        <button onClick={login}>Log in</button>
-    </div>
-    ) 
+            {userData.user ? (
+                <button onClick={logout} >Log out</button>
+            ) : (
+                <div>
+                   <button onClick={register}>Register</button>
+                   <button onClick={login}>Log in</button> 
+                </div>
+                )} 
+        </div>
+    ); 
 }
 
 export default AuthOptions
