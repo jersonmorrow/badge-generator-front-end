d9c05141f30acc136df0ee53483316c95aca5ad6
Jerson Q <solorsanolopez@gmail.com>
2020-12-10 19:39:43 -0800

    [Add] sign-up styles

--- src/features/auth/sign-up/index.jsx
+++ src/features/auth/sign-up/index.jsx
@@ -32,87 +32,136 @@ function SignUp() {
   };
 
   return (
-    <div>
-      <h2>Sign up</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="firstName">First Name</label>
-        <input
-          type="text"
-          name="firstName"
-          id="firstName"
-          autoComplete="firstName"
-          ref={register({ required: true })}
-        />
-        {errors.firstName && 'this input is required'}
+    <div className="column is-one-third">
+      <div className="box box-padding">
+        <h4 className="title is-4">Sign up</h4>
+        <div className="field has-addons-right">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-normal"
+                  type="text"
+                  name="firstName"
+                  id="signup-firstName"
+                  autoComplete="firstName"
+                  placeholder="First Name"
+                  ref={register({
+                    required: true,
+                  })}
+                />
+                {errors.firstName?.type === 'required' && (
+                  <p className="help is-danger">This is a required field</p>
+                )}
+              </div>
+            </div>
 
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          type="text"
-          name="lastName"
-          id="lastName"
-          autoComplete="lastName"
-          ref={register({ required: true })}
-        />
-        {errors.lastName && 'This input is required'}
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-normal"
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  id="signup-lastName"
+                  autoComplete="lastName"
+                  ref={register({
+                    required: true,
+                  })}
+                />
+                {errors.lastName?.type === 'required' && (
+                  <p className="help is-danger">This is a required field</p>
+                )}
+              </div>
+            </div>
 
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          autoComplete="email"
-          ref={register({
-            required: true,
-            pattern: {
-              value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-              message: 'error message',
-            },
-          })}
-        />
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-normal"
+                  type="email"
+                  name="email"
+                  id="signup-email"
+                  placeholder="Email"
+                  autoComplete="email"
+                  ref={register({
+                    required: true,
+                    pattern: {
+                      value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                      message: 'error message',
+                    },
+                  })}
+                />
 
-        {errors.email?.type === 'pattern' && (
-          <p className="error">
-            Please enter a valid email address. For example: amysmith@domain.com
-          </p>
-        )}
-        {errors.email?.type === 'required' && (
-          <p className="error">This is a required field</p>
-        )}
+                {errors.email?.type === 'pattern' && (
+                  <p className="help is-danger">
+                    Please enter a valid email address. For example:
+                    amysmith@domain.com
+                  </p>
+                )}
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          autoComplete="password"
-          ref={register({
-            required: true,
-            minLength: {
-              value: 8,
-              message: 'error message',
-            },
-          })}
-        />
-        {errors.password?.type === 'required' && (
-          <p className="error">This is a required field</p>
-        )}
-        {errors.password?.type === 'minLength' && (
-          <p className="error">Your password must be 8 characters long</p>
-        )}
-        <input
-          type="password"
-          name="passwordCheck"
-          id="passwordCheck"
-          placeholder="Verify password"
-          ref={register({ required: true })}
-        />
-        {errors.passwordCheck && 'This input is required'}
-        {formState.isSubmitted && (
-          <div className="success">Form submitted successfully</div>
-        )}
+                {errors.email?.type === 'required' && (
+                  <p className="help is-danger">This is a required field</p>
+                )}
+              </div>
+            </div>
 
-        <input type="submit" value="Sign up" disabled={!formState.isValid} />
-      </form>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-normal"
+                  type="password"
+                  name="password"
+                  id="signup-password"
+                  placeholder="Password"
+                  autoComplete="password"
+                  ref={register({
+                    required: true,
+                    minLength: {
+                      value: 8,
+                      message: 'error message',
+                    },
+                  })}
+                />
+                {errors.password?.type === 'required' && (
+                  <p className="help is-danger">This is a required field</p>
+                )}
+                {errors.password?.type === 'minLength' && (
+                  <p className="help is-danger">
+                    Your password must be 8 characters long
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-normal"
+                  type="password"
+                  name="passwordCheck"
+                  id="signup-passwordCheck"
+                  placeholder="Verify Password"
+                  ref={register({ required: true })}
+                />
+                {errors.passwordCheck?.type === 'required' && (
+                  <p className="help is-danger">This is a required field</p>
+                )}
+              </div>
+            </div>
+
+            <div className="control">
+              <button
+                type="submit"
+                className="button is-link"
+                disabled={!formState.isValid}
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
