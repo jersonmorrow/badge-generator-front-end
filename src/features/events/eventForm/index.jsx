c9c457bad3072aa401d88edabde99acc14ceb2a2
Jerson Q <solorsanolopez@gmail.com>
2020-12-18 01:11:36 -0800

    [Add] event form validations

--- src/features/events/eventForm/index.jsx
+++ src/features/events/eventForm/index.jsx
@@ -1,80 +1,148 @@
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
+import React, { useRef } from 'react';
+import ReactDatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { Controller, useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 function EventForm() {
-  const [startDate, setStartDate] = useState(new Date());
+  const { register, handleSubmit, control, errors } = useForm({
+    mode: 'onChanges',
+    reValidateMode: 'onChange',
+  });
+
+  const uploadedImage = useRef(null);
+  const imageUploader = useRef(null);
+
+  const handleImageUpload = (e) => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      const { current } = uploadedImage;
+      current.file = file;
+      reader.onload = (e) => {
+        current.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-    <div>
-      <form>
-        <div className="columns">
-          <div className="column">
-            <div className="field">
-              <div className="control">
-                <input
-                  className="input"
-                  type="text"
-                  placeholder="Event title"
-                />
-              </div>
-            </div>
-
-            <div className="field">
-              <div className="control">
-                <input className="input" type="text" placeholder="Organizer" />
-              </div>
-            </div>
-
-            <div className="field">
-              <div className="control">
-                <input className="input" type="text" placeholder="Location" />
-              </div>
-            </div>
-
-            <div className="field">
-              <label className="label">Date</label>
-              <DatePicker
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="columns">
+        <div className="column">
+          <div className="field">
+            <div className="control">
+              <input
                 className="input"
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
+                type="text"
+                placeholder="Event title"
+                name="title"
+                ref={register({
+                  required: true,
+                })}
               />
+              {errors.title?.type === 'required' && (
+                <p className="help is-danger">This is a required field</p>
+              )}
             </div>
           </div>
 
-          <div className="column">
-            <div className="field">
-              <div className="file has-name">
-                <label className="file-label">
-                  <input
-                    className="file-input"
-                    type="file"
-                    name="event-image"
-                  />
-                  <span className="file-cta">
-                    <span class="file-icon">
-                      <i class="fas fa-upload"></i>
-                    </span>
-                    <span class="file-label">Upload event image…</span>
-                  </span>
-                </label>
-              </div>
+          <div className="field">
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="Organizer"
+                name="organizer"
+                ref={register({
+                  required: true,
+                })}
+              />
+              {errors.organizer?.type === 'required' && (
+                <p className="help is-danger">This is a required field</p>
+              )}
             </div>
           </div>
 
-          <div className="field is-grouped">
-            <p className="control">
-              <button className="button is-danger is-normal">Cancel</button>
-            </p>
-            <p className="control">
-              <button className="button is-success is-normal">
-                Create event
-              </button>
-            </p>
+          <div className="field">
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="*Location"
+                name="location"
+                ref={register({
+                  required: true,
+                })}
+              />
+              {errors.location?.type === 'required' && (
+                <p className="help is-danger">This is a required field</p>
+              )}
+            </div>
+          </div>
+
+          <Controller
+            control={control}
+            register={register({ required: true })}
+            rules={{ required: true }}
+            name="date"
+            render={({ onChange, onBlur, value }) => (
+              <ReactDatePicker
+                className="input"
+                minDate={new Date()}
+                onChange={onChange}
+                onBlur={onBlur}
+                selected={value}
+                placeholderText="Click to select Date"
+                todayButton="Today"
+                isClearable
+              />
+            )}
+          />
+          {errors.date?.type === 'required' && (
+            <p className="help is-danger">This is a required field</p>
+          )}
+        </div>
+
+        <div className="column is-flex">
+          <div className="is-flex-direction-column is-align-items-center is-justify-content-center	">
+            <input
+              className="file-input"
+              type="file"
+              accept="image/*"
+              multiple="false"
+              name="img"
+              ref={(register, imageUploader)}
+              onChange={handleImageUpload}
+              style={{ display: 'none' }}
+            />
+            <div
+              className="box is-clickable is-flex is-justify-content-center is-align-items-center"
+              onClick={() => imageUploader.current.click()}
+            >
+              <img width="70" ref={uploadedImage} />
+            </div>
+            <h6 class="title is-6">Upload Image...</h6>
           </div>
         </div>
-      </form>
-    </div>
+
+        <div className="field is-grouped">
+          <Link to="/events" className="control">
+            <button className="button is-danger is-normal">Cancel</button>
+          </Link>
+
+          <p className="control">
+            <button type="submit" className="button is-success is-normal">
+              Create event
+            </button>
+          </p>
+        </div>
+      </div>
+    </form>
   );
 }
 
