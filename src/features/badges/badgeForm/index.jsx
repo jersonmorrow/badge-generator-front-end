aa44a7f13b0898d3f3304cc40842e57815ebdb0a
Jerson Q <solorsanolopez@gmail.com>
2021-01-08 12:37:56 -0800

    [Add] BadgeForm component

---
+++ src/features/badges/badgeForm/index.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function BadgeForm(props) {
+  const { onSubmit, register, errors } = props;
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="columns">
+        <div className="column">
+          <div className="field">
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="First Name"
+                name="name"
+                ref={register({
+                  required: true,
+                })}
+              />
+              {errors.name?.type === 'required' && (
+                <p className="help is-danger">This is a required field</p>
+              )}
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="Last Name"
+                name="lastName"
+                ref={register({
+                  required: true,
+                })}
+              />
+              {errors.lastName?.type === 'required' && (
+                <p className="help is-danger">This is a required field</p>
+              )}
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="Email"
+                name="email"
+                ref={register({
+                  required: true,
+                })}
+              />
+              {errors.email?.type === 'required' && (
+                <p className="help is-danger">This is a required field</p>
+              )}
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="URL badge image"
+                name="badgeImage"
+                ref={register}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="field is-grouped">
+        <Link to="/badges" className="control">
+          <button className="button is-danger is-normal">Cancel</button>
+        </Link>
+
+        <p className="control">
+          <button type="submit" className="button is-success is-normal">
+            Save
+          </button>
+        </p>
+      </div>
+    </form>
+  );
+}
+
+export default BadgeForm;
