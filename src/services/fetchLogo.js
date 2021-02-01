bfe82d6f41b26c687cfbf1b4035cd80338be68d0
Jerson Q <solorsanolopez@gmail.com>
2021-02-01 00:10:32 -0800

    [Update] fetchLogo service, and  BadgeEdit, BadgeDetails, NewBadge getEventLogo method

--- src/services/fetchLogo.js
+++ src/services/fetchLogo.js
@@ -1,11 +1,10 @@
 import api from '../api/api';
-import config from '../config/index';
 
 export const fetchLogo = async (setEventLogo, props) => {
   const response = await api.events.read(props.match.params.eventId);
   const eventLogo = response.eventImage;
   if (eventLogo) {
-    setEventLogo(`${config.apiUrl}/${eventLogo}`);
+    setEventLogo(eventLogo);
   }
   localStorage.setItem('event-logo', eventLogo);
 };
