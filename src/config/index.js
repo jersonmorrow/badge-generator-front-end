c05eb6057ef0ba720806ae215a5d5a2efdfb6551
Jerson Q <solorsanolopez@gmail.com>
2021-02-22 20:20:17 -0800

    [Fix] env configuration bug

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,5 @@
-import firebase from 'firebase';
-
 const config = {
   apiUrl: 'https://badge-generator-api.herokuapp.com',
-  firebaseApiKey: firebase.config().firebaseapi.key,
 };
 
 export default config;
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
-import config from '../config/index';
 
 const firebaseConfig = {
-  apiKey: config.firebaseApiKey,
-  authDomain: 'badge-generator-adb42.firebaseapp.com',
-  projectId: 'badge-generator-adb42',
-  storageBucket: 'badge-generator-adb42.appspot.com',
-  messagingSenderId: '497162240901',
-  appId: '1:497162240901:web:3ca5a1a897d5bbf4cebaec',
-  measurementId: 'G-NKV2BRNDJ5',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURMENTID,
 };
 
 firebase.initializeApp(firebaseConfig);
